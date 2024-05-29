da8fd8a7a902964705a401a5a6e0a365be8da1c8
AWieczarek <adamwie12@gmail.com>
2024-05-29 16:09:22 +0200

    Fix evaluation file

--- evaluate_model.py
+++ evaluate_model.py
@@ -11,8 +11,8 @@ y_test = test_data['review_overall']
 model = load_model('beer_review_sentiment_model.h5')
 
 tokenizer = Tokenizer(num_words=10000)
-tokenizer.fit_on_texts(X_test.values.ravel())
-X_test_seq = tokenizer.texts_to_sequences(X_test.values.ravel())
+tokenizer.fit_on_texts(X_test)
+X_test_seq = tokenizer.texts_to_sequences(X_test)
 
 X_test_pad = pad_sequences(X_test_seq, maxlen=100)
 
