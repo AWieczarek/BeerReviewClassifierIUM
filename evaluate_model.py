91c77ac4734739976fbee3598d0a264ec5acd114
AWieczarek <adamwie12@gmail.com>
2024-05-29 18:26:44 +0200

    Added metrics

--- evaluate_model.py
+++ evaluate_model.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support, mean_squared_error
+from math import sqrt
 
 test_data = pd.read_csv('beer_reviews_test.csv')
 X_test = test_data[['review_aroma', 'review_appearance', 'review_palate', 'review_taste']]
@@ -17,10 +19,23 @@ X_test_seq = tokenizer.texts_to_sequences(X_test)
 X_test_pad = pad_sequences(X_test_seq, maxlen=100)
 
 predictions = model.predict(X_test)
-print(f'Predictions shape: {predictions.shape}')
 
 if len(predictions.shape) > 1:
     predictions = predictions[:, 0]
 
 results = pd.DataFrame({'Predictions': predictions, 'Actual': y_test})
 results.to_csv('beer_review_sentiment_predictions.csv', index=False)
+
+y_pred = results['Predictions']
+y_test = results['Actual']
+y_test_binary = (y_test >= 3).astype(int)
+
+accuracy = accuracy_score(y_test_binary, y_pred.round())
+precision, recall, f1, _ = precision_recall_fscore_support(y_test_binary, y_pred.round(), average='micro')
+rmse = sqrt(mean_squared_error(y_test, y_pred))
+
+print(f'Accuracy: {accuracy}')
+print(f'Micro-avg Precision: {precision}')
+print(f'Micro-avg Recall: {recall}')
+print(f'F1 Score: {f1}')
+print(f'RMSE: {rmse}')
